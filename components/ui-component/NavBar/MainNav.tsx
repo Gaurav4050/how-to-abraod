bec6515c07a5d374ddfc0e6d9ab911141b204be8
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-05-05 16:12:28 +0530

    chore: Update MainNav component with ModeToggle and lazy loading for images

--- components/ui-component/NavBar/MainNav.tsx
+++ components/ui-component/NavBar/MainNav.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { ModeToggle } from "@/components/mode-changer";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 
@@ -13,7 +15,7 @@ export default function MainNav() {
         if(item === 'Home'){
             router.push('/')
         }else if(item === 'Search Course'){
-            router.push('/search')
+            router.push('/search-course')
         }else if(item === 'Login'){
             router.push('/login')
         }else if(item === 'Portal'){
@@ -22,8 +24,21 @@ export default function MainNav() {
     }
 
   return (
-    <div className="mr-4 hidden gap-2 md:flex">
-      {mainNavItems.map((item, index) => (
+    <div className="mr-4 hidden gap-2 md:flex w-full ">
+
+        <div
+        className="flex items-center justify-between w-full"
+        >
+            <div
+            className="flex cursor-pointer"
+            onClick={()=>{router.push('/')}}
+            >
+                <Image src="https://howtoabroad.com/wp-content/uploads/2021/09/cropped-HTA-logo_No-BG.png" alt="logo" width={150} height={100} />
+            </div>
+            <div
+            className="flex gap-4 items-center"
+            >
+            {mainNavItems.map((item, index) => (
         <Button key={index} variant="link"
         onClick={()=>{
             handleClicked(item)
@@ -32,6 +47,12 @@ export default function MainNav() {
           {item}
         </Button>
       ))}
+
+      <ModeToggle/>
+            </div>
+        </div>
+
+      
     </div>
   );
 }
