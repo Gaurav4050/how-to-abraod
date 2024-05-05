e31280a3c79eae96c5765072ecff8e4d258290a0
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-05-05 16:25:12 +0530

    feat: Update login page with authentication forms

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
 
-type Props = {}
+import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-const Login = (props: Props) => {
-  return (
-    <div>Login</div>
-  )
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
 }
 
-export default Login
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 lg:max-w-lg">
+              <Card>
+                <CardHeader className="space-y-1">
+                  <CardTitle className="text-2xl text-center">
+                    Sign in
+                  </CardTitle>
+                  <CardDescription className="text-center">
+                    Enter your email and password to login
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="" />
+                  </div>
+                  <div className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" />
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Checkbox id="terms" />
+                    <label
+                      htmlFor="terms"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </CardContent>
+                <CardFooter className="flex flex-col">
+                  <Button className="w-full">Login</Button>
+                  <p className="mt-2 text-xs text-center text-gray-700">
+                    {" "}
+                    Don't have an account?{" "}
+                    <span className=" text-blue-600 hover:underline">
+                      Sign up
+                    </span>
+                  </p>
+                </CardFooter>
+              </Card>
+            
+
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
